38985753bcd080e824413a793d81ba8d7b140640
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-03-28 00:48:37 +0200

    Fix inmemory login.

--- src/main/java/com/unloadbrain/blog/config/WebSecurityConfig.java
+++ src/main/java/com/unloadbrain/blog/config/WebSecurityConfig.java
@@ -1,37 +1,47 @@
 package com.unloadbrain.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
 
-        authManagerBuilder
-                .inMemoryAuthentication()
-                .withUser("user1")
-                .password("secret1")
-                .roles("USER")
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/**").permitAll().and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login?error=true")
+                .permitAll()
                 .and()
-                .withUser("admin1")
-                .password("secret1")
-                .roles("USER", "ADMIN");
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll().and()
+                .csrf().disable()
+                .headers().frameOptions().disable();
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.httpBasic().and()
-                .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/**").permitAll().and()
-                .csrf().disable()
-                .headers().frameOptions().disable();
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("1").
+                password("1").roles("ADMIN").build());
+        return manager;
     }
 }
