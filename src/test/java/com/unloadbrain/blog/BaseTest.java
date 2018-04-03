47a0ca69b838b41cfd6c2aed1635c884a06b9e6a
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-04-03 23:25:23 +0200

    Add base test classes.

---
+++ src/test/java/com/unloadbrain/blog/BaseTest.java
@@ -0,0 +1,12 @@
+package com.unloadbrain.blog;
+
+import org.junit.BeforeClass;
+
+import java.io.IOException;
+
+public abstract class BaseTest {
+
+    @BeforeClass
+    public static void init() throws IOException {
+    }
+}
