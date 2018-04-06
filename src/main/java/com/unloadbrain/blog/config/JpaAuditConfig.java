3bffa6f6f3d17a7ff174fda1337a1a1bd30c16d4
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-04-06 16:48:02 +0200

    Cleanup.

--- src/main/java/com/unloadbrain/blog/config/JpaAuditConfig.java
+++ src/main/java/com/unloadbrain/blog/config/JpaAuditConfig.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Configuration
