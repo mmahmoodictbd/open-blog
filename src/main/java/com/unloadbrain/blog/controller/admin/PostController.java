73d2320a4f230b9a8acad64d42fd8cd46b9fe645
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-04-10 22:27:35 +0200

    Add parmalink

--- src/main/java/com/unloadbrain/blog/controller/admin/PostController.java
+++ src/main/java/com/unloadbrain/blog/controller/admin/PostController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class PostController {
 
     @RequestMapping(path = "/admin/post", method = RequestMethod.GET)
-    public String postForm() {
+    public String showPostCreateEditPage() {
         return "admin/post";
+    }
+
+    @RequestMapping(path = "/admin/post", method = RequestMethod.POST)
+    public String createPost() {
+        return "redirect:/admin/post";
     }
 }
