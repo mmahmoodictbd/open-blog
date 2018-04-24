72d6eb2b32a8813bda12154e95e72ff9c4a18499
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-04-24 09:56:52 +0200

    Set timezone to UTC.

--- src/main/java/com/unloadbrain/blog/Application.java
+++ src/main/java/com/unloadbrain/blog/Application.java
@@ -3,10 +3,18 @@ package com.unloadbrain.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
