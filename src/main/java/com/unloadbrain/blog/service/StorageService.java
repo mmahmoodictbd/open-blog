aaa00b8d70e53ae18e2cd10f8b5ad249f81ef221
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-05-18 23:15:01 +0200

    Fix failing tests.

--- src/main/java/com/unloadbrain/blog/service/StorageService.java
+++ src/main/java/com/unloadbrain/blog/service/StorageService.java
@@ -1,8 +1,10 @@
 package com.unloadbrain.blog.service;
 
 import com.unloadbrain.blog.dto.FileIdentityDTO;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Service
 public class StorageService {
 
     public FileIdentityDTO store(MultipartFile file) {
