8070a4ee09216121fb3a1a95017fe1d4ed8c7a43
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-06-11 13:24:48 +0600

    Added additional properties in Site

--- src/main/java/com/unloadbrain/blog/domain/model/Site.java
+++ src/main/java/com/unloadbrain/blog/domain/model/Site.java
@@ -4,9 +4,16 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
+import javax.persistence.MapKeyColumn;
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -28,5 +35,11 @@ public class Site extends BaseEntity {
     @Column
     @Length(min = 1, max = 255)
     private String homeUrl;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name="site_additional_properties", joinColumns=@JoinColumn(name="site_id"))
+    @MapKeyColumn(name="property_key")
+    @Column(name="property_value")
+    private Map<String, String> additionalProperties = new HashMap<>();
 
 }
