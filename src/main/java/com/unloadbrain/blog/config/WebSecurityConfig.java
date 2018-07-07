2fcb48750319b853f46891e535167aeff9fcaf3c
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-07-08 00:31:11 +0200

    Fix admin login authentication.

--- src/main/java/com/unloadbrain/blog/config/WebSecurityConfig.java
+++ src/main/java/com/unloadbrain/blog/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -42,9 +43,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .antMatchers("/admin/**").hasAuthority("ADMIN")
-                .anyRequest().authenticated().and()
+                .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest().authenticated()
+                .antMatchers("/**").permitAll().and()
                 .formLogin()
                 .loginPage("/login")
                 .defaultSuccessUrl("/admin/dashboard")
@@ -59,4 +59,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable();
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(
+                        "/static/**",
+                        "/webjars/**"
+                );
+    }
 }
