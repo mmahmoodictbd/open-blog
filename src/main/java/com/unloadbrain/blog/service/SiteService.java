78fff7ad4ff19f28cd9094a2426e2832eaec000b
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-07-20 22:51:43 +0200

    Refactoring site service.

--- src/main/java/com/unloadbrain/blog/service/SiteService.java
+++ src/main/java/com/unloadbrain/blog/service/SiteService.java
@@ -1,62 +1,11 @@
 package com.unloadbrain.blog.service;
 
-import com.unloadbrain.blog.domain.model.Site;
-import com.unloadbrain.blog.domain.repository.SiteRepository;
 import com.unloadbrain.blog.dto.SiteQueryDTO;
 import com.unloadbrain.blog.dto.SiteUpdateCommandDTO;
-import com.unloadbrain.blog.exception.SiteNotFoundException;
-import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+public interface SiteService {
 
-@AllArgsConstructor
-@Service
-public class SiteService {
+    SiteQueryDTO getSite();
 
-    private SiteRepository siteRepository;
-
-    private ModelMapper modelMapper;
-
-    public SiteQueryDTO getSite() {
-
-        Optional<Site> site = getFirstSite();
-
-        throwExceptionWhenSiteNotFound(site);
-
-        return modelMapper.map(site.get(), SiteQueryDTO.class);
-
-    }
-
-    private void throwExceptionWhenSiteNotFound(Optional<Site> site) {
-        if (!site.isPresent()) {
-            throw new SiteNotFoundException("Site information should be pre persisted using init data mechanism.");
-        }
-    }
-
-    public void updateSite(SiteUpdateCommandDTO siteUpdateCommandDTO) {
-
-        Optional<Site> siteOptional = getFirstSite();
-
-        throwExceptionWhenSiteNotFound(siteOptional);
-
-        Site site = siteOptional.get();
-        modelMapper.map(siteUpdateCommandDTO, site);
-
-        siteRepository.save(site);
-    }
-
-    private Optional<Site> getFirstSite() {
-
-        List<Site> sites = siteRepository.findAll();
-
-        if (sites.size() > 0) {
-            return Optional.of(sites.get(0));
-        }
-
-        return Optional.empty();
-    }
-
+    void updateSite(SiteUpdateCommandDTO siteUpdateCommandDTO);
 }
---
+++ src/main/java/com/unloadbrain/blog/service/SiteServiceImpl.java
@@ -0,0 +1,69 @@
+package com.unloadbrain.blog.service;
+
+import com.unloadbrain.blog.domain.model.Site;
+import com.unloadbrain.blog.domain.repository.SiteRepository;
+import com.unloadbrain.blog.dto.SiteQueryDTO;
+import com.unloadbrain.blog.dto.SiteUpdateCommandDTO;
+import com.unloadbrain.blog.exception.SiteNotFoundException;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class SiteServiceImpl implements SiteService {
+
+    private static final String CACHE_KEY_SITE = "site";
+    private SiteRepository siteRepository;
+
+    private ModelMapper modelMapper;
+
+    @Override
+    @Cacheable(CACHE_KEY_SITE)
+    public SiteQueryDTO getSite() {
+
+        Optional<Site> site = getFirstSite();
+
+        throwExceptionWhenSiteNotFound(site);
+
+        return modelMapper.map(site.get(), SiteQueryDTO.class);
+
+    }
+
+    private void throwExceptionWhenSiteNotFound(Optional<Site> site) {
+        if (!site.isPresent()) {
+            throw new SiteNotFoundException("Site information should be pre persisted using init data mechanism.");
+        }
+    }
+
+    @Override
+    @CacheEvict(value = CACHE_KEY_SITE, allEntries = true)
+    public void updateSite(SiteUpdateCommandDTO siteUpdateCommandDTO) {
+
+        Optional<Site> siteOptional = getFirstSite();
+
+        throwExceptionWhenSiteNotFound(siteOptional);
+
+        Site site = siteOptional.get();
+        modelMapper.map(siteUpdateCommandDTO, site);
+
+        siteRepository.save(site);
+    }
+
+    private Optional<Site> getFirstSite() {
+
+        List<Site> sites = siteRepository.findAll();
+
+        if (sites.size() > 0) {
+            return Optional.of(sites.get(0));
+        }
+
+        return Optional.empty();
+    }
+
+}
