711ce7e102c471e7b334181f832ac966d77b11dc
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-07-24 22:51:31 +0200

    Improve exception with http status.

--- src/main/java/com/unloadbrain/blog/exception/DraftPostNotFoundException.java
+++ src/main/java/com/unloadbrain/blog/exception/DraftPostNotFoundException.java
@@ -1,5 +1,9 @@
 package com.unloadbrain.blog.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such draft post")
 public class DraftPostNotFoundException extends RuntimeException {
 
     public DraftPostNotFoundException(String message) {
--- src/main/java/com/unloadbrain/blog/exception/PublishedPostNotFoundException.java
+++ src/main/java/com/unloadbrain/blog/exception/PublishedPostNotFoundException.java
@@ -1,5 +1,9 @@
 package com.unloadbrain.blog.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such published post")
 public class PublishedPostNotFoundException extends RuntimeException {
 
     public PublishedPostNotFoundException(String message) {
