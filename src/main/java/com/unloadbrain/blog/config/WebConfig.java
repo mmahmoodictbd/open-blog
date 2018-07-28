e0434faf2c92241e686a250b801ac8a7b984c6d4
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-07-28 21:59:30 +0200

    Remove deprecated class.

--- src/main/java/com/unloadbrain/blog/config/WebConfig.java
+++ src/main/java/com/unloadbrain/blog/config/WebConfig.java
@@ -3,11 +3,11 @@ package com.unloadbrain.blog.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
