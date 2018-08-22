bd4d9a281993d1bf203b5d0aa68fffc647df92ee
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-08-23 00:13:24 +0200

    Added load resource test for bad url.

--- src/test/java/com/unloadbrain/blog/service/StorageServiceTest.java
+++ src/test/java/com/unloadbrain/blog/service/StorageServiceTest.java
@@ -1,15 +1,27 @@
 package com.unloadbrain.blog.service;
 
+import com.unloadbrain.blog.exception.BadUrlException;
 import com.unloadbrain.blog.exception.FileNotFoundException;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.core.io.UrlResource;
 
+import java.net.MalformedURLException;
+
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({StorageServiceImpl.class})
 public class StorageServiceTest {
 
-    private final StorageService storageService;
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+    private final StorageService storageService;
 
     public StorageServiceTest() {
         this.storageService = new StorageServiceImpl();
@@ -32,5 +44,22 @@ public class StorageServiceTest {
 
     }
 
+    @Test
+    public void shouldThrowBadUrlExceptionWhenResourceUrlIsMalformed() throws Exception {
 
+        // Given
+
+        thrown.expect(BadUrlException.class);
+        thrown.expectMessage("Provided URL [anything] is malformed.");
+
+        whenNew(UrlResource.class).withAnyArguments().thenThrow(new MalformedURLException());
+
+        // When
+
+        storageService.loadFileAsResource("anything");
+
+        // Then
+        // Expect test to be passed.
+
+    }
 }
