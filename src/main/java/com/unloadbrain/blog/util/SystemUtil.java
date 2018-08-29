1b5307fde6c56b63efdd1e3eec5149221d075769
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-08-29 12:41:13 +0200

    Added get user home and get openblog home methods.

---
+++ src/main/java/com/unloadbrain/blog/util/SystemUtil.java
@@ -0,0 +1,33 @@
+package com.unloadbrain.blog.util;
+
+/**
+ * System related utils
+ */
+public class SystemUtil {
+
+    /**
+     * Constant for user's home directory key
+     */
+    private static final String USER_HOME = "user.home";
+
+    /**
+     * OpenBlog home directory under user home
+     */
+    private static final String OPENBLOG_HOME = ".openblog";
+
+    /**
+     * Return user home directory path.
+     * Example: /Users/mossaddeque
+     */
+    public String getUserHome() {
+        return System.getProperty(USER_HOME);
+    }
+
+    /**
+     * Return OpenBlog home directory path.
+     * Example: /Users/mossaddeque/.openblog
+     */
+    public String getOpenBlogHome() {
+        return System.getProperty(USER_HOME) + "/" + OPENBLOG_HOME;
+    }
+}
