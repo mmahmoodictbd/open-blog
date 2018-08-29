5518a90ad1256e6659bd751839b375449263310e
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-08-29 13:32:10 +0200

    Add original exception in IORuntimeException constructor.

--- src/main/java/com/unloadbrain/blog/exception/IORuntimeException.java
+++ src/main/java/com/unloadbrain/blog/exception/IORuntimeException.java
@@ -5,4 +5,8 @@ public class IORuntimeException extends RuntimeException {
     public IORuntimeException(String message) {
         super(message);
     }
+
+    public IORuntimeException(String message, Exception original) {
+        super(message, original);
+    }
 }
