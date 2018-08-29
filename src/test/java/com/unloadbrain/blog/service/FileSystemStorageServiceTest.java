f050af2a5ecdc48504d824bf13db22fd4b982b4b
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-08-29 13:34:30 +0200

    Added tests to cover all scenario in file system storage service.

--- src/test/java/com/unloadbrain/blog/service/FileSystemStorageServiceTest.java
+++ src/test/java/com/unloadbrain/blog/service/FileSystemStorageServiceTest.java
@@ -2,11 +2,14 @@ package com.unloadbrain.blog.service;
 
 import com.unloadbrain.blog.exception.FileNotFoundException;
 import com.unloadbrain.blog.exception.IORuntimeException;
+import com.unloadbrain.blog.util.DateUtil;
 import com.unloadbrain.blog.util.FileSystemFileWriterUtil;
+import com.unloadbrain.blog.util.SystemUtil;
 import com.unloadbrain.blog.util.UuidUtil;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.mockito.ArgumentCaptor;
 import org.springframework.core.io.AbstractResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -14,19 +17,25 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class FileSystemStorageServiceTest {
 
+    private final MultipartFile fileMock;
     private final FileSystemFileWriterUtil fileSystemFileWriterUtilMock;
     private final ResourceLoader resourceLoaderMock;
+    private final DateUtil dateUtilMock;
     private final UuidUtil uuidUtilMock;
+    private final SystemUtil systemUtilMock;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -34,10 +43,17 @@ public class FileSystemStorageServiceTest {
     private final StorageService storageService;
 
     public FileSystemStorageServiceTest() {
+        this.fileMock = mock(MultipartFile.class);
         this.resourceLoaderMock = mock(ResourceLoader.class);
-        this.fileSystemFileWriterUtilMock = mock(FileSystemFileWriterUtil.class);
+        this.dateUtilMock = mock(DateUtil.class);
         this.uuidUtilMock = mock(UuidUtil.class);
-        this.storageService = new FileSystemStorageService(resourceLoaderMock, fileSystemFileWriterUtilMock, uuidUtilMock);
+        this.systemUtilMock = mock(SystemUtil.class);
+        this.fileSystemFileWriterUtilMock = mock(FileSystemFileWriterUtil.class);
+        this.storageService = new FileSystemStorageService(resourceLoaderMock,
+                dateUtilMock,
+                uuidUtilMock,
+                systemUtilMock,
+                fileSystemFileWriterUtilMock);
     }
 
     @Test
@@ -108,6 +124,42 @@ public class FileSystemStorageServiceTest {
     }
 
     @Test
+    public void shouldLookForResourceInOpenBlogHomeFilesDirectory() {
+
+        // Given
+
+        ArgumentCaptor<String> filePathCaptor = ArgumentCaptor.forClass(String.class);
+
+        Resource resource = new AbstractResource() {
+
+            @Override
+            public boolean exists() {
+                return true;
+            }
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+
+            @Override
+            public InputStream getInputStream() throws IOException {
+                return null;
+            }
+        };
+        when(resourceLoaderMock.getResource(anyString())).thenReturn(resource);
+
+        when(systemUtilMock.getOpenBlogHome()).thenReturn("/home/user/.openblog");
+
+        // When
+        storageService.loadFileAsResource("anything");
+
+        // Then
+        verify(resourceLoaderMock).getResource(filePathCaptor.capture());
+        assertEquals("file:/home/user/.openblog/files/anything", filePathCaptor.getValue().toString());
+    }
+
+    @Test
     public void shouldThrowIORuntimeExceptionWhenCouldNotCreateUploadDirectory() throws Exception {
 
         // Given
@@ -119,9 +171,108 @@ public class FileSystemStorageServiceTest {
 
         // When
 
-        storageService.store(mock(MultipartFile.class));
+        storageService.store(fileMock);
 
         // Then
         // Expect test to be passed.
+    }
+
+    @Test
+    public void shouldThrowIORuntimeExceptionWhenFileBytesCouldNotBeRead() throws Exception {
+
+        // Given
+
+        thrown.expect(IORuntimeException.class);
+        thrown.expectMessage("Could not read file.");
+
+        when(fileMock.getOriginalFilename()).thenReturn("my picture.jpg");
+        doThrow(new IOException()).when(fileMock).getBytes();
+
+        // When
+        storageService.store(fileMock);
+
+        // Then
+        // Expect test to be passed.
+    }
+
+    @Test
+    public void shouldThrowIORuntimeExceptionWhenFileCanNotBeWritten() throws Exception {
+
+        // Given
+
+        thrown.expect(IORuntimeException.class);
+        thrown.expectMessage("Could not write file.");
+
+        when(fileMock.getOriginalFilename()).thenReturn("my picture.jpg");
+        doThrow(new IOException()).when(fileSystemFileWriterUtilMock).write(any(), any());
+
+        // When
+        storageService.store(fileMock);
+
+        // Then
+        // Expect test to be passed.
+    }
+
+    @Test
+    public void shouldCreateUploadDirectoryUsingCurrentDate() throws Exception {
+
+        // Given
+
+        ArgumentCaptor<Path> pathArgumentCaptor = ArgumentCaptor.forClass(Path.class);
+
+        when(fileMock.getOriginalFilename()).thenReturn("my picture.jpg");
+        when(dateUtilMock.getCurrentYearMonthDateString()).thenReturn("2018/8/29");
+        when(systemUtilMock.getOpenBlogHome()).thenReturn("/home/user/.openblog");
+
+        // When
+        storageService.store(fileMock);
+
+        // Then
+        verify(fileSystemFileWriterUtilMock).createDirectories(pathArgumentCaptor.capture());
+        assertEquals("/home/user/.openblog/files/2018/8/29", pathArgumentCaptor.getValue().toString());
+    }
+
+    @Test
+    public void shouldFileNameBeGeneratedFromUuidAndSluggedFileName() throws Exception {
+
+        // Given
+
+        ArgumentCaptor<Path> pathArgumentCaptor = ArgumentCaptor.forClass(Path.class);
+
+        when(fileMock.getOriginalFilename()).thenReturn("my picture.jpg");
+        when(dateUtilMock.getCurrentYearMonthDateString()).thenReturn("2018/8/29");
+        when(systemUtilMock.getOpenBlogHome()).thenReturn("/home/user/.openblog");
+        when(uuidUtilMock.getUuid()).thenReturn("uuid123");
+
+        // When
+        storageService.store(fileMock);
+
+        // Then
+        verify(fileSystemFileWriterUtilMock).write(pathArgumentCaptor.capture(), any());
+        assertEquals("/home/user/.openblog/files/2018/8/29/uuid123_my-picture.jpg",
+                pathArgumentCaptor.getValue().toString());
+    }
+
+    @Test
+    public void shouldMultipartFileBytesToBeWritten() throws Exception {
+
+        // Given
+
+        ArgumentCaptor<byte[]> bytesArgumentCaptor = ArgumentCaptor.forClass(byte[].class);
+
+        when(fileMock.getOriginalFilename()).thenReturn("my picture.jpg");
+        when(fileMock.getBytes()).thenReturn("pictureBytes".getBytes());
+
+        when(dateUtilMock.getCurrentYearMonthDateString()).thenReturn("2018/8/29");
+        when(systemUtilMock.getOpenBlogHome()).thenReturn("/home/user/.openblog");
+        when(uuidUtilMock.getUuid()).thenReturn("uuid123");
+
+        // When
+        storageService.store(fileMock);
+
+        // Then
+        verify(fileSystemFileWriterUtilMock).write(any(), bytesArgumentCaptor.capture());
+        assertEquals("pictureBytes",
+                new String(bytesArgumentCaptor.getValue(), "UTF-8"));
     }
 }
