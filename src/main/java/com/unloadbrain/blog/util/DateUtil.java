82ce3787b1d2484cecd12a13c5d547e6fc52911b
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-08-29 12:39:52 +0200

    Added date as string and get calendar methods.

--- src/main/java/com/unloadbrain/blog/util/DateUtil.java
+++ src/main/java/com/unloadbrain/blog/util/DateUtil.java
@@ -1,15 +1,37 @@
 package com.unloadbrain.blog.util;
 
+import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * Provides date related utils
+ */
 public class DateUtil {
 
     /**
      * Return current date.
-     * @return
      */
     public Date getCurrentDate() {
         return new Date();
     }
 
+    /**
+     * Return Calendar object
+     */
+    public Calendar getCalendar() {
+        return Calendar.getInstance();
+    }
+
+    /**
+     * Return current year, month and date as string separated by forward slash.
+     * For example: 2018/12/1 as 1st of December 2018
+     */
+    public String getCurrentYearMonthDateString() {
+        Calendar calendar = getCalendar();
+        return calendar.get(Calendar.YEAR)
+                + "/"
+                + (calendar.get(Calendar.MONTH) + 1)
+                + "/"
+                + calendar.get(Calendar.DATE);
+    }
 }
