698b13447bf4ed244d4c57ee58fc9dc535d7671a
mmahmoodictbd <mmahmood.ict.bd@gmail.com>
2018-08-29 12:21:43 +0200

    Added method doc in DateUtil.

--- src/main/java/com/unloadbrain/blog/util/DateUtil.java
+++ src/main/java/com/unloadbrain/blog/util/DateUtil.java
@@ -4,7 +4,12 @@ import java.util.Date;
 
 public class DateUtil {
 
+    /**
+     * Return current date.
+     * @return
+     */
     public Date getCurrentDate() {
         return new Date();
     }
+
 }
